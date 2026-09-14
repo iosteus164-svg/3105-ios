caace97c73041ea7364072324552b2f09d43eef5
ms6719987-spec <iosteus164@gmail.com>
2026-09-14 04:07:28 -0300

    h

--- ThreeOneOSFive/App.swift
+++ ThreeOneOSFive/App.swift
@@ -97,8 +97,12 @@ class AppState: ObservableObject {
         )
     }
 
-    var isSupported: Bool { if AppInfo.osVersion == "26.6.2" { return true }
-        unsupportedMessage == nil }
+    var isSupported: Bool {
+        if AppInfo.osVersion == "26.6.2" {
+            return true
+        }
+        return unsupportedMessage == nil
+    }
 
     func detectSupport() {
         let v = AppInfo.versionTuple
