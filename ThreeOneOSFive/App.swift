34bb9d2d92d63f22c2b4dd96edee417304ddebd7
ms6719987-spec <iosteus164@gmail.com>
2026-09-15 00:31:34 -0300

    ll

--- ThreeOneOSFive/App.swift
+++ ThreeOneOSFive/App.swift
@@ -47,7 +47,6 @@ struct ThreeOneOSFiveApp: App {
                 .sheet(isPresented: $showAttribution) {
                     DisplayAttributionSheet()
                 }
-,
                         secondaryButton: .cancel(Text(language.text("update.dismiss"))) {
                             AppUpdateChecker.dismiss(version: offer.version)
                         }
@@ -64,8 +63,8 @@ struct ThreeOneOSFiveApp: App {
                     patchDraftCoordinator.presentImport(url)
                 }
         }
-    }
-}
+    
+
 
 class AppState: ObservableObject {
     @Published var exploitStatus: ExploitStatus = .notStarted
